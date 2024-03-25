9b6a752bc3fd2054d969a87a3694c6f5f67b55f0
tue.it <tue.it@bkk.paragon-worldwide.com>
2024-03-25 15:40:56 +0700

    4. Add a UI In this step you place a user interface over the scene created in the previous step. This includes the title, the difficulty-selector buttons, and the all-important Start button.

--- lib/title_screen/title_screen.dart
+++ lib/title_screen/title_screen.dart
@@ -2,13 +2,39 @@ import 'package:flutter/material.dart';
 
 import '../assets.dart';
 import '../styles.dart'; // Add this import
+import 'title_screen_ui.dart'; // Add this import
 
-class TitleScreen extends StatelessWidget {
+class TitleScreen extends StatefulWidget {
   const TitleScreen({super.key});
 
-  final _finalReceiveLightAmt = 0.7; // Add this attribute
-  final _finalEmitLightAmt = 0.5; // And this attribute
+  @override
+  State<TitleScreen> createState() => _TitleScreenState();
+}
 
+class _TitleScreenState extends State<TitleScreen> {
+  Color get _emitColor =>
+      AppColors.emitColors[_difficultyOverride ?? _difficulty];
+  Color get _orbColor =>
+      AppColors.orbColors[_difficultyOverride ?? _difficulty];
+
+  /// Currently selected difficulty
+  int _difficulty = 0;
+
+  /// Currently focused difficulty (if any)
+  int? _difficultyOverride;
+
+  void _handleDifficultyPressed(int value) {
+    setState(() => _difficulty = value);
+  }
+
+  void _handleDifficultyFocused(int? value) {
+    setState(() => _difficultyOverride = value);
+  }
+
+  final _finalReceiveLightAmt = 0.7;
+  // Add this attribute
+  final _finalEmitLightAmt = 0.5;
+  // And this attribute
   @override
   Widget build(BuildContext context) {
     final orbColor = AppColors.orbColors[0]; // Add this final variable
@@ -71,6 +97,16 @@ class TitleScreen extends StatelessWidget {
               imgSrc: AssetPaths.titleFgEmit,
               color: emitColor,
               lightAmt: _finalEmitLightAmt,
+            ),
+
+            /// UI
+            Positioned.fill(
+              // Add from here...
+              child: TitleScreenUi(
+                difficulty: _difficulty,
+                onDifficultyFocused: _handleDifficultyFocused,
+                onDifficultyPressed: _handleDifficultyPressed,
+              ),
             ),
           ],
         ),
